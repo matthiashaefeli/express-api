d0325655fc3065b20477ed22c82f9547ef424a7f
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-23 16:23:31 -0600

    add mongo db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.v4tb6.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instace')
+});
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to mongo', err)
+});
 
 app.get('/', (req, res) =>{
   res.send('Hi there');
