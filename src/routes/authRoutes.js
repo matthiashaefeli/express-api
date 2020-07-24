b1805c3108ffea189b161c564e95a1cd8831d12c
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-24 15:05:34 -0600

    add user model

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongosse');
+const User = mongoose.model('User');
 
 const router = express.Router();
 
 router.post('/signup', (req, res) => {
-res.send('post request');
+  console.log(req.body)
+  res.send('post request');
 });
 
 module.exports = router;
